3255350a70e79f81bc6786f7c2c52624238a6264
Charles Dietrich <charles.m.dietrich@gmail.com>
2012-02-19 14:03:45 -0700

    setting up Arduino

---
+++ Arduino/Boulderino/Boulderino.cpp
@@ -0,0 +1,1 @@
+#include "Boulderino.h"
---
+++ Arduino/Boulderino/Boulderino.h
@@ -0,0 +1,18 @@
+#define PB0 8
+#define PB1 9
+#define PB2 10
+#define PB4 12
+#define PB5 13
+#define PD0 0
+#define PD1 1
+#define PD2 2
+#define PD4 4
+#define PD7 7
+
+#define PC5 5
+#define PC4 4
+#define PC3 3
+#define PC2 2
+#define PC1 1
+#define ADC6 6
+#define ADC7 7
